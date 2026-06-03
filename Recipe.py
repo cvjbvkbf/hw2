ae763c443d1eca01fd6f3a7521a90ada3b89a2c9
vlad_seledkin <vlseseledkin@edu.hse.ru>
2026-06-03 14:26:32 +0300

    написал класс Recipe

---
+++ Recipe.py
@@ -0,0 +1,37 @@
+from Ingredient import Ingredient
+
+class Recipe:
+    
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = []
+        for ingredient in ingredients:
+            self.add_ingredient(ingredient)
+        
+    def add_ingredient(self, ingredient: Ingredient):
+        for ingred in self.ingredients:
+            if ingred.__eq__(ingredient):
+                ingred.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+            
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+    
+    def scale(self, ratio: float):
+        ingredients_new = []
+        for ingredient in self.ingredients:
+            ingredients_new.append(Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit))
+        return Recipe(self.title, ingredients_new)
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        if not self.ingredients:
+            return f"У {self.title} пока нет ингредиентов"
+        ingredients_str = []
+        for ingredient in self.ingredients:
+            ingredients_str.append(str(ingredient))
+        return f"{self.title}\n{ingredients_str}"
